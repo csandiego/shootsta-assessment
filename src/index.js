54f231302bccd27ca8b4b25bcd7cf707a1f37499
Christopher San Diego <csandiego@gmail.com>
2023-11-26 06:06:23 +0800

    Add code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext, useContext, useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const languages = ['JavaScript', 'Python']
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const LanguageContext = createContext({ current: 0, toggle: () => { } })
+
+const App = () => {
+  const [current, setCurrent] = useState(0)
+  const toggle = () => setCurrent((current + 1) % languages.length) // Increment and wrap
+  return (
+    <LanguageContext.Provider value={{ current, toggle }}>
+      <Main />
+    </LanguageContext.Provider>
+  )
+}
+
+const Main = () => {
+  const { current, toggle } = useContext(LanguageContext)
+  return (
+    <div>
+      <p>Language {languages[current]}</p>
+      <button onClick={() => toggle()}>Toggle</button>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
